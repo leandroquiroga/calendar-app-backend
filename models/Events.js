99459f3318e14db1392ee54a510a9902014540c9
leandroquiroga <leandroquiroga9514@gmail.com>
2022-05-22 20:31:59 -0300

    modify: Has been modified the keys (_v, _id) of the property of the database

--- models/Events.js
+++ models/Events.js
@@ -3,23 +3,31 @@ const { Schema, model } = require('mongoose');
 const EventsSchema = Schema({
   title: {
     type: String,
-    require: true
+    required: true
   },
   notes: {
     type: String,
   },
   start: {
     type: Date,
-    require: true
+    required: true
   },
   end: {
     type: Date,
-    require: true
+    required: true
   },
   user: {
     type: Schema.Types.ObjectId,
     ref: 'Users',
+    required: true
   },
 });
 
+EventsSchema.method('toJSON', function () {
+  // Extraemos toda la informacion del objeto de nuestra base de datos,
+  // se modifica el nombre _id por id. 
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
 module.exports = model('Events', EventsSchema);
