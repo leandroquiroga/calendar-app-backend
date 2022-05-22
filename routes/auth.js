afb950438e56c26220112abc01aba04ad5ce0fd0
leandroquiroga <leandroquiroga9514@gmail.com>
2022-05-22 20:36:01 -0300

    validation: Has been created the validation of the field with chek of express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,33 @@
-/* 
-  Rutas de usuarios
-  port + /auth
-*/
-
+/*  Rutas de usuarios --> port + /auth */
 const { Router } = require('express');
-const { renewToken, loginUser, registerUser, registerGETUser } = require('../controllers/auth');
+const {check } = require('express-validator')
+const { renewToken, loginUser, registerUser } = require('../controllers/auth');
+const { validatorJWT } = require('../middlewares/validatorJWT');
+const { fieldValidator } = require('./../middlewares/fieldValidator');
 const router = Router();
 
-// Revalida el codigo token
-router.get('/renew', renewToken)
-
-// Pagina de registro de usuario
-router.post('/register', registerUser);
-
+// Revalida el JSON Web Token
+router.get(
+  '/renew',
+  validatorJWT,
+  renewToken
+);
+// Pagina de registro de const {request} = require('express')
+router.post(
+  '/register',
+  // Como se utiliza varios middlewares utilizamos corchetes
+  [check('name', 'Debe colocar un nombre de usuario').not().isEmpty().isString()],
+  [check('email', 'Email no valido').isEmail()],
+  [check('password', 'La contraseña debe tener un minimo de 6 caracteres').isLength({ min: 6 })],
+  fieldValidator,
+  registerUser
+);
 //pagina de login de usuario
-router.post('/login', loginUser)
-
+router.post(
+  '/login',
+  [check('email', 'Email no valido').isEmail()],
+  [check('password', 'La contraseña no es valida').isLength({ min: 6 })],
+  fieldValidator,
+  loginUser
+);
 module.exports = router;
