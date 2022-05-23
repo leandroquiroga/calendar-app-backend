6b0ada6136d09efc3ddb27d98a39bb4e4f0a710e
leandroquiroga <leandroquiroga9514@gmail.com>
2022-05-23 14:40:41 -0300

    fix: Has been fixed a problem the parameters of the function validationResult

--- middlewares/fieldValidator.js
+++ middlewares/fieldValidator.js
@@ -4,9 +4,11 @@ const { validationResult } = require('express-validator');
 
 const fieldValidator = (req, res = response, next) => {
 
-  const errors = validationResult(req) 
+  const errors = validationResult(req.body);
+
   // Si hay errores retorna un objeto especificando el error 
   if (!errors.isEmpty()) { 
+    console.log(errors);
     return res.status(400).json({
       ok: false,
       errors: errors.mapped(),
